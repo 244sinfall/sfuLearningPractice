e951d6a4535ca0286dc110dd7bc784bdc51f0009
244sinfall <dimafilin6@icloud.com>
2022-06-25 19:15:05 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,202 @@
+# Задание:
+# Дан двумерный массив размером n*m, заполненный случайным образом.
+# 1. Заменить максимальный элемент каждой строки нулем
+# 2. Вставить после каждого столбца, содержащего максимальный элемент массива, столбец из
+# нулей.
+# 3. Удалить все столбцы, в которых встретится нечетный положительный элемент.
+# 4. Поменять местами первый и предпоследний столбцы.
+
+# Начинаем написание кода с тестов. Директория tests. Создаем пустые функции по заданию.
+
+
+import random
+
+
+def menu():
+    using_array = []
+    print("""
+    Приложение, разработанное в ходе прохождения учебной практики.
+    Выполнил: Студент ЗКИ21-16Б. Филин Дмитрий Алексеевич.
+    Вариант №7.
+    Задание:
+Дан двумерный массив размером n*m, заполненный случайным образом.
+1. Заменить максимальный элемент каждой строки нулем
+2. Вставить после каждого столбца, содержащего максимальный элемент массива, столбец из
+нулей.
+3. Удалить все столбцы, в которых встретится нечетный положительный элемент.
+4. Поменять местами первый и предпоследний столбцы.         
+""")
+    while True:
+        if not using_array:
+            n = m = 0
+            min_random = max_random = -999999999999999
+            print('Для начала работы необходимо сгенерировать массив.\n'
+                  'Это можно сделать по нескольким параметрам.\n'
+                  'Для начала введите длинну строки (размер подмассива)\n'
+                  'Макисмальное значение: 1000, минимальное: 3\n'
+                  'Введите значение:')
+            while int(m) < 3 or int(m) > 1000:
+                try:
+                    m = int(input())
+                except ValueError:
+                    print('Неверное значение, попробуйте еще раз:')
+                    continue
+            print('Введите количество столбцов (размер массива)\n'
+                  'Макисмальное значение: 1000, минимальное: 3\n'
+                  'Введите значение:')
+            while int(n) < 3 or int(n) > 1000:
+                try:
+                    n = int(input())
+                except ValueError:
+                    print('Неверное значение, попробуйте еще раз:')
+                    continue
+            print('Введите минимальное значение, возможное для двумерного массива\n'
+                  'Минимальное значение: -1.000.000.000, максимальное: 500.000.000\n'
+                  'Введите значение:')
+            while int(min_random) < -1000000000 or int(min_random) > 500000000:
+                try:
+                    min_random = int(input())
+                except ValueError:
+                    print('Неверное значение, попробуйте еще раз:')
+                    continue
+            print('Введите максимальное значение, возможное для двумерного массива\n'
+                  f'Минимальное значение: {min_random+10}, максимальное: 1.000.000.000\n'
+                  'Введите значение:')
+            while int(max_random) < (min_random+10) or int(max_random) > 1000000000:
+                try:
+                    max_random = int(input())
+                except ValueError:
+                    print('Неверное значение, попробуйте еще раз:')
+                    continue
+            using_array = generate_2d_array(n, m, min_random, max_random)
+        print('Ваш текущий массив:')
+        for row in using_array:
+            print(row)
+        print('Выберите операцию по пункту меню:\n'
+              '1. Заменить максимальный элемент каждой строки нулем\n'
+              '2. Вставить после каждого столбца, содержащего максимальный элемент массива'
+              'столбец из нулей\n'
+              '3. Удалить все столбцы, в которых встретиться нечетный положительный элемент\n'
+              '4. Поменять местами первый и предпоследний столбцы\n'
+              '5. Перегенерировать массив\n'
+              '0. Выйти\n')
+        choice = -1
+        while int(choice) < 0 or int(choice) > 5:
+            try:
+                choice = int(input())
+            except ValueError:
+                print('Неверное значение, попробуйте еще раз:')
+                continue
+        if choice == 0:
+            exit(0)
+        if choice == 1:
+            using_array = replace_every_max_element_with_zero(using_array)
+            continue
+        if choice == 2:
+            using_array = insert_zero_column_after_column_with_max_element(using_array)
+            continue
+        if choice == 3:
+            using_array = delete_column_with_odd_positive_element(using_array)
+            continue
+        if choice == 4:
+            using_array = replace_first_with_before_last_column(using_array)
+            continue
+        if choice == 5:
+            using_array = []
+            continue
+
+
+def generate_2d_array(n: int, m: int, min_random: int, max_random: int) -> [[int]]:
+    """Генерирует двумерный массив случайных чисел. n - количество массивов в массиве, m - размер одного массива
+    min - минимальное число для генерации, max - макисмальное число для генерации. Размер массива не больше 1000x1000"""
+    # Выходим, если n и m не валидны
+    if n <= 0 or m <= 0 or n > 1000 or m > 1000:
+        return []
+    generated_array = []
+    for _ in range(0, n):
+        second_dimension_array = []
+        for _ in range(0, m):
+            second_dimension_array.append(random.randint(min_random, max_random))
+        generated_array.append(second_dimension_array)
+    return generated_array
+
+
+def replace_every_max_element_with_zero(twod_array: [[int]]) -> [[int]]:
+    """Заменяет максимальный элемент каждой строки нулем. (Каждого массива с числами внутри массива)"""
+    for row in twod_array:
+        row[row.index(max(row))] = 0
+    return twod_array
+
+
+def insert_zero_column_after_column_with_max_element(twod_array: [[int]]) -> [[int]]:
+    """Вставляет столбец из 0 (добавление 0 в каждый массив в двумерном массиве на определенный индекс), если
+    предыдущий столбец содержит максимальное значение в массиве"""
+    # Получаем максимальное число из всего массива
+    max_value = get_absolute_max_value_in_array(twod_array)
+    # Находим индексы, после которых вставляем нули
+    indexes_to_place_zeros_after = set()
+    for row in twod_array:
+        if max_value in row:
+            last_found_index = -1
+            while True:
+                try:
+                    last_found_index = row.index(max_value, last_found_index + 1)
+                except ValueError:
+                    break
+                indexes_to_place_zeros_after.add(last_found_index)
+    # Вставляем нули
+    for row in twod_array:
+        inserted = 0
+        for index in indexes_to_place_zeros_after:
+            if index+inserted == len(row)-1:
+                row.append(0)
+                inserted += 1
+            else:
+                row.insert(index+inserted+1, 0)
+                inserted += 1
+    return twod_array
+
+
+def get_absolute_max_value_in_array(twod_array: [[int]]) -> int:
+    tmp_max = 0
+    for row in twod_array:
+        tmp_max_local = max(row)
+        if tmp_max_local > tmp_max:
+            tmp_max = tmp_max_local
+    return tmp_max
+
+
+def delete_column_with_odd_positive_element(twod_array: [[int]]) -> [[int]]:
+    """Удаляет столбец (один и тот же индекс каждого массива в массиве, если среди этих значений есть положительное,
+    нечетное число)"""
+    # Находим все столбцы, которые нужно удалить
+    columns_to_delete = set()
+    for row in twod_array:
+        for element in row:
+            if element > 0 and element % 2 != 0:
+                columns_to_delete.add(row.index(element))
+    # Удаляем столбцы
+    for row in twod_array:
+        deleted = 0
+        for index in columns_to_delete:
+            try:
+                del row[index-deleted]
+            except IndexError:
+                break
+            deleted += 1
+    return twod_array
+
+
+def replace_first_with_before_last_column(twod_array: [[int]]) -> [[int]]:
+    """Заменяет первый с предпоследним столбцом (один и тот же индекс каждого массива в массиве)"""
+    # Выходим, если в массиве меньше 3 массивов. Перестановка бессмысленна
+    if len(twod_array) < 3:
+        return twod_array
+    before_last_index = len(twod_array[0]) - 2
+    for row in twod_array:
+        row[0], row[before_last_index] = row[before_last_index], row[0]
+    return twod_array
+
+
+if __name__ == "__main__":
+    menu()
---
+++ tests/tests.py
@@ -0,0 +1,109 @@
+import unittest
+from main import generate_2d_array, replace_every_max_element_with_zero, \
+    insert_zero_column_after_column_with_max_element, delete_column_with_odd_positive_element, \
+    replace_first_with_before_last_column
+
+
+class MyTestCase(unittest.TestCase):
+    def test_generating(self):
+        print("Testing Generator:")
+        array_empty = generate_2d_array(0, 0, -10000, 10000)
+        self.assertEqual(array_empty == [[]], True)  # add assertion here
+        array_wrong = generate_2d_array(-1, -1, -99912391239, 48523849392)
+        self.assertEqual(array_wrong == [[]], True)  # add assertion here
+        array_extremely_high_values = generate_2d_array(5, 5, -999999999999999999, 999999999999999999)
+        self.assertEqual(len(array_extremely_high_values) == 5, True)  # add assertion here
+        self.assertEqual(len(array_extremely_high_values[1]) == 5, True)  # add assertion here
+        print(array_extremely_high_values)
+
+        # array_extremely_large = generate_2d_array(9999999999, 999999999, -1000, 1000)
+        # self.assertEqual(len(array_extremely_large) == 9999999999, True)  # add assertion here
+        # self.assertEqual(len(array_extremely_large[555555]) == 9999999999, True)  # add assertion here
+        # ^ рантайм падает, введу ограничение по значениям
+        array_normal = generate_2d_array(15, 15, -100, 100)
+        self.assertEqual(len(array_normal) == 15, True)  # add assertion here
+        self.assertEqual(len(array_normal[3]) == 15, True)  # add assertion here
+        print(array_normal)
+
+    def test_replacing_max_value_with_zero(self):
+        print("Testing replacing max value with zero:")
+        test_array = [[-1000, 500, 1000, 5000, 1500000],
+                      [-2123, 5235, 4357, 232, 1],
+                      [-45753231, 543, 22323, 56734, 735674],
+                      [-1, 2222, 31000, 441241, 6346575],
+                      [-63465, 2323212312, 344562334, 453433243, 222222222222]]
+        tested_array = replace_every_max_element_with_zero(test_array)
+        print(tested_array)
+        self.assertEqual(len(tested_array) == 5, True)  # add assertion here
+        self.assertEqual(len(tested_array[4]) == 5, True)  # add assertion here
+        self.assertEqual(tested_array[0][4] == 0, True)  # add assertion here
+        self.assertEqual(tested_array[1][1] == 0, True)  # add assertion here
+        self.assertEqual(tested_array[2][4] == 0, True)  # add assertion here
+        self.assertEqual(tested_array[3][4] == 0, True)  # add assertion here
+        self.assertEqual(tested_array[4][4] == 0, True)  # add assertion here
+
+    def test_adding_zero_column_after_max_value_column(self):
+        print("Testing column add after max value column:")
+        test_array = [[0, 0, 0, 0, 1],
+                      [0, 0, 0, 1, 0],
+                      [0, 1, 0, 0, 0],
+                      [1, 0, 0, 0, 0],
+                      [1, 1, 0, 0, 1]]
+        test_array2 = [[235, 12356, 23221, 53456, 122],
+                       [4235, 45634, 7676, 124, 5464],
+                       [145125, 32333, 19283, -1235, 3432],
+                       [3887, 29393, 93939393, 124124, 544354],
+                       [1, 1, 0, 0, 1]]
+        tested_array = insert_zero_column_after_column_with_max_element(test_array)
+        tested_array2 = insert_zero_column_after_column_with_max_element(test_array2)
+        print(tested_array)
+        print(tested_array2)
+        self.assertEqual(len(tested_array[0]) == 9, True)
+        self.assertEqual(tested_array[0][8] == 0, True)
+        self.assertEqual(tested_array[3][8] == 0, True)
+        self.assertEqual(len(tested_array2[0]) == 6, True)
+        self.assertEqual(tested_array[0][3] == 0, True)
+        self.assertEqual(tested_array[3][3] == 0, True)
+
+    def test_delete_column_with_odd_positive_element(self):
+        print("Testing odd and positive column deletion:")
+        test_array = [[0, 2, 4, 6, 9],
+                      [0, 2, 3, 6, 8],
+                      [0, 2, 4, 6, 8],
+                      [0, 2, 4, 6, 8],
+                      [0, 2, 4, 6, 8],
+                      [0, 1, 4, 6, 8],
+                      [0, 2, 4, 6, 8]]
+        test_array2 = [[15922, 54933, 2030],
+                       [12222, 12, 222],
+                       [3333, 2, 2]]
+        tested_array = delete_column_with_odd_positive_element(test_array)
+        tested_array2 = delete_column_with_odd_positive_element(test_array2)
+        print(tested_array)
+        print(tested_array2)
+        self.assertEqual(len(tested_array[0]) == 2, True)
+        self.assertEqual(tested_array[0][0] == 0, True)
+        self.assertEqual(len(tested_array2[0]) == 1, True)
+        self.assertEqual(tested_array2[0][0] == 2030, True)
+        self.assertEqual(tested_array2[2][0] == 2, True)
+
+    def test_replace_first_with_before_last_column(self):
+        print("Testing replacing first column with before last one:")
+        test_array = [[]]
+        test_array2 = [[1, 2, 3], [2, 3, 4], [4, 5, 6]]
+        test_array3 = [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6], [3, 4, 5, 6, 7]]
+        tested_array = replace_first_with_before_last_column(test_array)
+        tested_array2 = replace_first_with_before_last_column(test_array2)
+        tested_array3 = replace_first_with_before_last_column(test_array3)
+        print('Tested Array 1: ' + str(tested_array))
+        print('Tested Array 2: ' + str(tested_array2))
+        print('Tested Array 3: ' + str(tested_array3))
+        self.assertEqual(tested_array == [[]], True)
+        self.assertEqual(tested_array2[0][1] == 1, True)
+        self.assertEqual(tested_array2[1][0] == 3, True)
+        self.assertEqual(tested_array3[2][0] == 6, True)
+        self.assertEqual(tested_array3[2][3] == 3, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
